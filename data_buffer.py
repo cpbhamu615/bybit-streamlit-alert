478830630ea866c2f2f29fe2fd1f2734f7ffcd86
cpbhamu615 <130212315+cpbhamu615@users.noreply.github.com>
2025-07-21 14:22:09 +0530

    add

--- data_buffer.py
+++ data_buffer.py
@@ -2,18 +2,5 @@
 
 from collections import defaultdict
 
-# Store candle data for each symbol
 candle_data = defaultdict(list)
-
-def update_candles(symbol, new_candle):
-    data = candle_data[symbol]
-
-    # Check if it's a new candle or update existing
-    if data and data[-1]["start"] == new_candle["start"]:
-        data[-1] = new_candle
-    else:
-        data.append(new_candle)
-
-    # Keep only last 100 candles
-    if len(data) > 100:
-        data.pop(0)
+prices = {}
